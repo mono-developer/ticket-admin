e5fcd4ce0c5c8c411c73f1f330bcdef74ffcc7f9
Developer <minorumadam@hotmail.com>
2018-06-14 10:00:11 -0300

    fixed server issue

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
